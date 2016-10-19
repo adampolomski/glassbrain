c8c1d393a4ffe166c37ac1da80fa996586e75897
Adam Połomski <adam.polomski@gmail.com>
2016-10-19 19:15:37 +0200

    Initial changes.

--- glassbrain/glassbrain.py
+++ glassbrain/glassbrain.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_restful import Resource, Api
 from predictor import PredictorRepository
 from price import PriceHistoryBuilder, PriceEventRepository
+from db import getDb
 
 app = Flask(__name__)
 api = Api(app)
 
-predictorRepository = PredictorRepository()
-priceEventRepository = PriceEventRepository()
+predictorRepository = PredictorRepository(db.getDb())
+priceEventRepository = PriceEventRepository(db.getDb())
 
 class Predictions(Resource):
     def get(self, predictor_id):
@@ -21,6 +22,9 @@ class PriceEvents(Resource):
         for event in priceEvents:
             event.extract(priceHistoryBuilder)
         return priceHistoryBuilder.build()
+    
+    def post(self):
+        return
 
 api.add_resource(Predictions, '/v1/predictors/<predictor_id>/predictions')
 api.add_resource(PriceEvents, '/v1/predictors/<predictor_id>/prices')
