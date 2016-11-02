917a1fcebd498d8e375616c4c4d67d09773c7e97
Adam Połomski <adam.polomski@gmail.com>
2016-11-02 22:44:01 +0100

    Brute force knot detection.

--- glassbrain/domain/predictor.py
+++ glassbrain/domain/predictor.py
@@ -1,4 +1,6 @@
 from sklearn import linear_model
+import math
+
 '''
 @author: Adam Polomski
 '''
@@ -12,18 +14,39 @@ class LinearSplinesPredictor(object):
         self._weights = weights
         self._intercept = intercept
 
-    def predictAll(self, X):
+    def predict_all(self, X):
         return map(self.predict, X)
         
     def predict(self, x):
         xMapped = [x] + map( lambda knot: max(0, x - knot), self._knots)
         return round(sum( w * k for (w, k) in zip(xMapped, self._weights) ) + self._intercept, 2)
 
-def train(prices, knots):
-    X = [[x[0], max(0, x[0]-knots[0]), max(0, x[0]-knots[1])] for x in prices]
+def fit(prices, step_base = 40):  
+    best_score = None
+    best_predictor = None
+    
+    step = int(math.ceil(prices.shape[0] / step_base))
+    
+    for l_knot in range(1, int(prices[-3,0]), step):
+        for r_knot in range(l_knot + 1, int(prices[-2,0]), step):
+            X = _knotify([l_knot, r_knot], prices[:,0])
+            clf = _regression(X, prices[:,1])
+            score = clf.score(X, prices[:,1])
+            
+            if best_predictor is None or score > best_score:
+                best_predictor = LinearSplinesPredictor([l_knot, r_knot], clf.coef_, clf.intercept_)
+                best_score = score
+                
+    return best_predictor
+
+def _knotify(knots, days):
+    X = [[x, max(0, x-knots[0]), max(0, x-knots[1])] for x in days]
+    return X
+
+def _regression(X, prices):
     clf = linear_model.LinearRegression();
-    clf.fit(X, prices[:,1])
-    return LinearSplinesPredictor(knots, clf.coef_, clf.intercept_)
+    clf.fit(X, prices)
+    return clf
 
 class PredictorRepository(object):
         
--- tests/domain/test_predictor.py
+++ tests/domain/test_predictor.py
@@ -6,14 +6,14 @@ def test_should_predict():
     linearPredictor = predictor.LinearSplinesPredictor([1,2,3], [1,2,3,4])
     
     # when, then
-    assert [50, 170] == linearPredictor.predictAll([7, 19])
+    assert [50, 170] == linearPredictor.predict_all([7, 19])
     
 def test_should_train():
     # given
     data = np.loadtxt("tests/domain/s5prices.txt")
     
     # when
-    linearPredictor = predictor.train(data, [90, 420]);
+    linearPredictor = predictor.fit(data, 10);
     
     # then
-    assert 3037.21 == linearPredictor.predict(2)
+    assert 13300 > np.mean((linearPredictor.predict_all(data[:,0]) - data[:,1]) ** 2)
