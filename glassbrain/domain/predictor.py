830e6fa2d8864802449a7f799118f171c59123f6
Adam Połomski <adam.polomski@gmail.com>
2016-11-03 13:03:50 +0100

    Predictor and tests.

--- glassbrain/domain/predictor.py
+++ glassbrain/domain/predictor.py
@@ -21,17 +21,14 @@ class LinearSplinesPredictor(object):
         xMapped = [x] + map( lambda knot: max(0, x - knot), self._knots)
         return round(sum( w * k for (w, k) in zip(xMapped, self._weights) ) + self._intercept, 2)
 
-def fit(prices, step_base = 40):  
+def fit(prices, step, regression):
     best_score = None
     best_predictor = None
-    
-    step = int(math.ceil(prices.shape[0] / step_base))
     
     for l_knot in range(1, int(prices[-3,0]), step):
         for r_knot in range(l_knot + 1, int(prices[-2,0]), step):
             X = _knotify([l_knot, r_knot], prices[:,0])
-            clf = _regression(X, prices[:,1])
-            score = clf.score(X, prices[:,1])
+            (clf, score) = regression(X, prices[:,1])
             
             if best_predictor is None or score > best_score:
                 best_predictor = LinearSplinesPredictor([l_knot, r_knot], clf.coef_, clf.intercept_)
@@ -43,10 +40,10 @@ def _knotify(knots, days):
     X = [[x, max(0, x-knots[0]), max(0, x-knots[1])] for x in days]
     return X
 
-def _regression(X, prices):
+def linear_regression(X, prices):
     clf = linear_model.LinearRegression();
     clf.fit(X, prices)
-    return clf
+    return (clf, clf.score(X, prices))
 
 class PredictorRepository(object):
         
