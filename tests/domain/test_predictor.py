1e663f93a1f367d1a76ea3272fe33a7037fa5ef9
Adam Połomski <adam.polomski@gmail.com>
2016-12-16 12:20:44 +0100

    Moved to python 3.

--- tests/domain/test_predictor.py
+++ tests/domain/test_predictor.py
@@ -1,28 +1,34 @@
 from glassbrain.domain import predictor
 import numpy as np
 from mock import Mock
-    
+
 def test_should_predict():
     # given
-    linearPredictor = predictor.LinearSplinesPredictor([1,2,3], [1,2,3,4])
+    linear_predictor = predictor.LinearSplinesPredictor([1,2,3], [1,2,3,4])
     
     # when, then
-    assert linearPredictor.predict([7, 19]) == [50, 170]
-    
+    resp = linear_predictor.predict([7, 19])
+
+    # then
+    assert list(resp) == [50, 170]
+
+
 def test_should_iterate_over_knots():
     # given
     data = np.array([[x, x] for x in range(1,10)])
-    def mock_regression(X, prices):
-        if X == predictor._knotify([4,7], data[:,0]):
-            return (Mock(coef_ = [1], intercept_ = 1), 1)
-        return (Mock(coef_ = [1], intercept_ = 0), 0)
+
+    def mock_regression(x, prices):
+        if x == predictor._knotify([4, 7], data[:, 0]):
+            return Mock(coef_ = [1], intercept_=1), 1
+        return Mock(coef_ = [1], intercept_ = 0), 0
     
     # when
-    linearPredictor = predictor.fit(data, 1, mock_regression);
+    linear_predictor = predictor.fit(data, 1, mock_regression);
     
     # then
-    assert linearPredictor.predict(0) == 1
-    
+    assert linear_predictor.predict(0) == 1
+
+
 def test_should_fit_linear_regression():
     # given
     data = np.loadtxt("tests/domain/s5prices.txt")
